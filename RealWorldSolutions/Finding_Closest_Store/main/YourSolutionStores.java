ad019836b07766db2254bcded81cb8f57c22f485
Amjad <meisamamjad@gmail.com>
2017-08-08 22:18:13 -0400

    Just added YouSolution class

---
+++ RealWorldSolutions/Finding_Closest_Store/main/YourSolutionStores.java
@@ -0,0 +1,21 @@
+package main;
+
+public class YourSolutionStores extends Stores{
+	
+	public YourSolutionStores(){
+		super();
+	}
+
+	@Override
+	public void build(StoresLocation[] allLocations) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public StoresLocation getNearest(double lng, double lat) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
