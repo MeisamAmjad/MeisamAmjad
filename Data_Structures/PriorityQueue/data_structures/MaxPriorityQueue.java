54b6ba204b9fa12f0dcef9e6a07b2621c8e8ecfe
Amjad <meisamamjad@gmail.com>
2017-08-10 15:36:06 -0400

    Added some comments explaining these 2 classess would also be implemented by Randomized Meldable Heap class

--- Data_Structures/PriorityQueue/data_structures/MaxPriorityQueue.java
+++ Data_Structures/PriorityQueue/data_structures/MaxPriorityQueue.java
@@ -9,7 +9,7 @@ import java.util.Collection;
 import java.util.Iterator;
 
 /**
- * <p>******** This class had been implemented by MinHeap.java class from <b>BinaryHeap</b> {@link MaxHeap} ************** </p>
+ * <p>******** This class had been implemented by MinHeap.java class from <b>BinaryHeap</b> {@link MaxHeap} and maxMeldableHeap.java class from <b>RandomizedMeldableHeap</b> {@link MaxMeldableHeap}************** </p>
  * <p>a priority queue is an abstract data type which is like a regular queue or stack data structure, but where additionally each element has a "priority" associated with it.</br>
  *  In a priority queue, an element with high priority is served before an element with low priority. If two elements have the same priority, they are served according to </br>
  *  their order in the queue. While priority queues are often implemented with heaps, they are conceptually distinct from heaps. A priority queue is an abstract concept</br>
