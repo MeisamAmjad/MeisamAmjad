70718f1fcfd617c83470721c9cb93fd1ad7dc2bb
meisamamjad <amjadm@miamioh.edu>
2018-02-12 23:48:59 -0500

    mad the two_sum cpp file

--- LeetCodes/01_two_sum/two_sum.cpp
+++ LeetCodes/01_two_sum/two_sum.cpp
@@ -1,25 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
- * File:   two_sum.cpp
- * Author: Meijad
- *
- * Created on February 12, 2018, 11:39 PM
- */
-
-#include <cstdlib>
-
-using namespace std;
-
-/*
+ * ##########################################################################
+ * Given an array of integers, return indices of the two numbers such that they 
+ * add up to a specific target. You may assume that each input would have 
+ * exactly one solution, and you may not use the same element twice.
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * ##########################################################################
  * 
+ * 
+ * File:   two_sum.cpp
+ * Author: Meisam Amjad amjadm@miamioh.edu
+ *
+ * Copyright (c) 2018
  */
-int main(int argc, char** argv) {
 
+#include <cstdio>
+#include <vector>
+#include <unordered_set>
+
+class two_sum {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_set<int> tempList;
+        for (auto element = nums.begin(); element != nums.end(); ++element) {
+            if (tempLis.insert(*element);
+            
+        }
+    }
+}
+
+int main(int argc, char** argv) {
     return 0;
 }
 
