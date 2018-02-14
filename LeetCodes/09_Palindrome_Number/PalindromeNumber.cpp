dad7e2d49dd8c5903293757fb7bd620c61b5df91
meisamamjad <amjadm@miamioh.edu>
2018-02-14 12:07:21 -0500

    Just made the Palindrome cpp class

---
+++ LeetCodes/09_Palindrome_Number/PalindromeNumber.cpp
@@ -0,0 +1,38 @@
+/* Copyright (C) 2017 amjadm@miamiOH.edu
+ * 
+ * ##########################################################################
+ * Determine whether an integer is a palindrome. Do this without extra space.
+ * 
+ * Some hints:
+ * Could negative integers be palindromes? (ie, -1)
+ * If you are thinking of converting the integer to string, note the 
+ * restriction of using extra space.
+ * 
+ * You could also try reversing an integer. However, if you have solved the
+ * problem "Reverse Integer", you know that the reversed integer might
+ * overflow. How would you handle such case?
+ * ##########################################################################
+ * 
+ * 
+ * File:   PalindromeNumber.cpp
+ * 
+ * Author: Meisam Amjad amjadm@miamioh.edu
+ * 
+ * Time in leetCode =  ms
+ * Rank in leetCode = beats % in that runtime range.
+ */
+
+#include <cstdlib>
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+    }
+};
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
