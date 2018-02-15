21d746cedbdcdf67b67ed7f909e5d9ca2d1de032
meisamamjad <amjadm@miamioh.edu>
2018-02-15 17:20:45 -0500

    finished and had it run just fine

--- LeetCodes/13_Roman_to_Integer/romanToInteger.cpp
+++ LeetCodes/13_Roman_to_Integer/romanToInteger.cpp
@@ -10,22 +10,44 @@
  * 
  * Author: Meisam Amjad amjadm@miamioh.edu
  * 
- * Time in leetCode =   ms
- * Rank in leetCode = beats % in that runtime range.
+ * Time in leetCode =  73 ms
+ * Rank in leetCode = beats 61.34% in that runtime range.
  */
+
+#include <assert.h>
 #include <string>
+#include <unordered_map>
 
-using str = std::string;
-
-class RomanToInteger {
+class RomanToInteger {    
 public:
-    int romanToInt(str s) {
-        
+    int romanToInt(std::string s) {
+        std::unordered_map<char, int> romanSigns = {{'I', 1}, {'V', 5}, 
+                                                    {'X', 10}, {'L', 50},
+                                                    {'C', 100}, {'D', 500}, 
+                                                    {'M', 1000}};
+        int prevNum = 0,  currentNum, result = 0;
+        for (int i = s.size(); i >= 0; --i) {
+            currentNum = romanSigns[s[i]];
+            if (currentNum < prevNum)
+                result -= currentNum;
+            else
+                result += currentNum;
+            prevNum = currentNum;            
+        }
+        return result;
     }
 };
 
 int main(int argc, char** argv) {
-
+    RomanToInteger test;
+    assert(test.romanToInt("II") == 2);
+    assert(test.romanToInt("IV") == 4);
+    assert(test.romanToInt("IX") == 9);
+    assert(test.romanToInt("LXIII") == 63);
+    assert(test.romanToInt("XC") == 90);
+    assert(test.romanToInt("XCIX") == 99);
+    assert(test.romanToInt("LXXIV") == 74);
+    assert(test.romanToInt("XXXVIII") == 38);
     return 0;
 }
 
