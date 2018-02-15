e5f0707d1f580cfaf284166a3db7101096e08df8
meisamamjad <amjadm@miamioh.edu>
2018-02-15 16:00:13 -0500

    just started roman to int project

---
+++ LeetCodes/13_Roman_to_Integer/nbproject/Makefile-Release.mk
@@ -0,0 +1,82 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux
+CND_DLIB_EXT=so
+CND_CONF=Release
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/romanToInteger.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk 13_Roman_to_Integer_opt
+
+13_Roman_to_Integer_opt: ${OBJECTFILES}
+	${LINK.cc} -o 13_Roman_to_Integer_opt ${OBJECTFILES} ${LDLIBSOPTIONS} -lboost_system -lpthread -lmysqlpp
+
+${OBJECTDIR}/romanToInteger.o: romanToInteger.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -Wall -std=c++14 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/romanToInteger.o romanToInteger.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
---
+++ LeetCodes/13_Roman_to_Integer/romanToInteger.cpp
@@ -0,0 +1,31 @@
+/* Copyright (C) 2017 amjadm@miamiOH.edu
+ * 
+ * ##########################################################################
+ * Given a roman numeral, convert it to an integer.
+ * 
+ * Input is guaranteed to be within the range from 1 to 3999.
+ * ##########################################################################
+ * 
+ * File:   RomanToInteger.cpp
+ * 
+ * Author: Meisam Amjad amjadm@miamioh.edu
+ * 
+ * Time in leetCode =   ms
+ * Rank in leetCode = beats % in that runtime range.
+ */
+#include <string>
+
+using str = std::string;
+
+class RomanToInteger {
+public:
+    int romanToInt(str s) {
+        
+    }
+};
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
