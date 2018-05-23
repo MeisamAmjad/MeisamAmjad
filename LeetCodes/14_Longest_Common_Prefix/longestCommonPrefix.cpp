b2ae2df9980da3c83c8dffa0f59624f056b5ba34
meisamamjad <amjadm@miamioh.edu>
2018-05-23 17:58:50 -0400

    finished and had it tested just fine

--- LeetCodes/14_Longest_Common_Prefix/longestCommonPrefix.cpp
+++ LeetCodes/14_Longest_Common_Prefix/longestCommonPrefix.cpp
@@ -22,29 +22,53 @@
  * 
  * Author: Meisam Amjad amjadm@miamioh.edu
  * 
- * Time in leetCode =   ms
- * Rank in leetCode = beats % in that runtime range.
+ * Time in leetCode = 4 ms
+ * Rank in leetCode = beats 100% in that runtime range.
  */
 
+#include <assert.h>
 #include <iostream>
 #include <string>
 #include <vector>
 
-static auto strs = []() {
+static auto __ = []() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
     return "";
-} ();
+}();
 
 class Solution {
 public:
     std::string longestCommonPrefix(std::vector<std::string>& strs) {
-        
+        if (strs.size() == 0) return "";
+        if (strs.size() == 1) return strs[0];     
+        size_t index = 0;
+        std::string temp = strs[0];
+        while (index < temp.size()) {
+            for (size_t item = 1; item < strs.size(); ++item)
+                if (strs[item].size() > index) {
+                    if (temp[index] != strs[item][index])
+                        return temp.substr(0, index);
+                } else {
+                    return temp.substr(0, index);
+                }
+            ++index;
+        }
+        return temp.substr(0, index);
     }
 };
 
 int main(int argc, char* argv[]) {
-
+    std::vector<std::string> sample1 = {"flower", "flow", "flight"};
+    std::vector<std::string> sample2 = {"dog", "racecar", "car"};
+    std::vector<std::string> sample3 = {"aa", "a"};
+    Solution lCP;
+    // std::cout << lCP.longestCommonPrefix(sample1) << std::endl;
+    // std::cout << lCP.longestCommonPrefix(sample2) << std::endl;
+    // std::cout << lCP.longestCommonPrefix(sample3) << std::endl;
+    assert(lCP.longestCommonPrefix(sample1) == "fl");
+    assert(lCP.longestCommonPrefix(sample2) == "");
+    assert(lCP.longestCommonPrefix(sample3) == "a");
     return 0;
 }
 
