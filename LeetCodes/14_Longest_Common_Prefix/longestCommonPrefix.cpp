2f3951d54b88358693381e5ef5b6e82b84a26c44
meisamamjad <amjadm@miamioh.edu>
2018-05-22 20:35:22 -0400

    started working on that

---
+++ LeetCodes/14_Longest_Common_Prefix/longestCommonPrefix.cpp
@@ -0,0 +1,50 @@
+/* Copyright (C) 2017 amjadm@miamiOH.edu
+ * 
+ * ##########################################################################
+ * Write a function to find the longest common prefix string amongst an array 
+ * of strings.
+ * If there is no common prefix, return an empty string "".
+ * 
+ * Example 1:
+ * Input: ["flower","flow","flight"]
+ * Output: "fl"
+ * 
+ * Example 2:
+ * Input: ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ * 
+ * Note:
+ * All given inputs are in lowercase letters a-z.
+ * ##########################################################################
+ * 
+ * File:   longestCommonPrefix.cpp
+ * 
+ * Author: Meisam Amjad amjadm@miamioh.edu
+ * 
+ * Time in leetCode =   ms
+ * Rank in leetCode = beats % in that runtime range.
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+static auto strs = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    return "";
+} ();
+
+class Solution {
+public:
+    std::string longestCommonPrefix(std::vector<std::string>& strs) {
+        
+    }
+};
+
+int main(int argc, char* argv[]) {
+
+    return 0;
+}
+
