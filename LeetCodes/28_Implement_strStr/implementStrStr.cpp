6c98e4752a60f2a40ec52071ee44f810f0ec99fe
meisamamjad <amjadm@miamioh.edu>
2018-05-26 22:07:00 -0400

    just finished and had it tested just fine

--- LeetCodes/28_Implement_strStr/implementStrStr.cpp
+++ LeetCodes/28_Implement_strStr/implementStrStr.cpp
@@ -24,22 +24,40 @@
  * 
  * Author: Meisam Amjad amjadm@miamioh.edu
  * 
- * Time in leetCode =  ms
- * Rank in leetCode = beats % in that runtime range.
+ * Time in leetCode = 4 ms
+ * Rank in leetCode = beats 100% in that runtime range.
  */
 
 #include <assert.h>
 #include <iostream>
 #include <string>
 
+static auto ___ = [] () {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    return 0;
+}();
+
 class Solution {
 public:
     int strStr(std::string haystack, std::string needle) {
-        return 0;
+        if (needle.empty()) return 0;
+        if (haystack.empty()) return -1;
+        std::hash<std::string> strHash;
+        size_t needle_hash = strHash(needle);
+        for (size_t i = 0, jmp = needle.size(); i + jmp <= haystack.size(); ++i)
+            if (strHash(haystack.substr(i, jmp)) == needle_hash)
+                return i;
+        return -1;
     }
 };
 
 int main(int argc, char** argv) {
+    std::string sample1 = "hello", n1 = "ll";
+    std::string sample2 = "aaaaa", n2 = "bba";
+    Solution test;
+    assert(test.strStr(sample1, n1) == 2);
+    assert(test.strStr(sample2, n2) == -1);
     return 0;
 }
 
