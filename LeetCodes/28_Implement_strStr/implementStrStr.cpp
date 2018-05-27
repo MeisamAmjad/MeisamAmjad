79d77c3dadb6dc73699973da2cc1019a889cfa9f
meisamamjad <amjadm@miamioh.edu>
2018-05-26 21:30:17 -0400

    Just started

---
+++ LeetCodes/28_Implement_strStr/implementStrStr.cpp
@@ -0,0 +1,45 @@
+/* Copyright (C) 2018 amjadm@miamiOH.edu
+ * 
+ * ##########################################################################
+ * Return the index of the first occurrence of needle in haystack, or -1 if
+ * needle is not part of haystack.
+ * 
+ * Example 1:
+ * Input: haystack = "hello", needle = "ll"
+ * Output: 2
+ * 
+ * Example 2:
+ * Input: haystack = "aaaaa", needle = "bba"
+ * Output: -1
+ * 
+ * Clarification:
+ * What should we return when needle is an empty string? This is a great
+ * question to ask during an interview.
+ * 
+ * For the purpose of this problem, we will return 0 when needle is an
+ * empty string. This is consistent to C's strstr() and Java's indexOf().
+ * ##########################################################################
+ * 
+ * File:   implementStrStr.cpp
+ * 
+ * Author: Meisam Amjad amjadm@miamioh.edu
+ * 
+ * Time in leetCode =  ms
+ * Rank in leetCode = beats % in that runtime range.
+ */
+
+#include <assert.h>
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+    int strStr(std::string haystack, std::string needle) {
+        return 0;
+    }
+};
+
+int main(int argc, char** argv) {
+    return 0;
+}
+
