8c50d1a0e1d436c614094210df5b41ce074b8e3f
meisamamjad <amjadm@miamioh.edu>
2018-05-27 22:38:22 -0400

    finished and had it tested with the best time I got

--- LeetCodes/53_Maximum_Subarray/maximumSubarray.cpp
+++ LeetCodes/53_Maximum_Subarray/maximumSubarray.cpp
@@ -18,21 +18,43 @@
  * 
  * Author: Meisam Amjad amjadm@miamioh.edu
  * 
- * Time in leetCode =  ms
- * Rank in leetCode = beats % in that runtime range.
+ * Time in leetCode = 6 ms
+ * Rank in leetCode = beats 100% in that runtime range.
  */
 
 #include <assert.h>
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+static auto ___ = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    return 0;
+}();
 
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        return 0;
+    int maxSubArray(std::vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int finalMax = nums[0];
+        for (int i = 1, currMax = finalMax; i < nums.size(); 
+                                    getMax(currMax, finalMax, nums[i++])) {}
+        return finalMax;
+    }
+    
+private:
+    inline
+    void getMax(int& currMax, int&finalMax, int num) {
+        currMax  = std::max(num, currMax + num);
+        finalMax = std::max(finalMax, currMax);
     }
 };
 
 int main(int argc, char** argv) {
+    std::vector<int> sample1 = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    Solution test;
+    assert(test.maxSubArray(sample1) == 6);
     return 0;
 }
 
