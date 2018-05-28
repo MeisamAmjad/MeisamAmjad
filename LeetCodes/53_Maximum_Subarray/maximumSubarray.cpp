2e4bdc33731ca9b11fd38d108392228edda9ffb3
meisamamjad <amjadm@miamioh.edu>
2018-05-27 22:05:20 -0400

    just added this project

---
+++ LeetCodes/53_Maximum_Subarray/maximumSubarray.cpp
@@ -0,0 +1,38 @@
+/* Copyright (C) 2018 amjadm@miamiOH.edu
+ * 
+ * ##########################################################################
+ * Given an integer array nums, find the contiguous subarray (containing at 
+ * least one number) which has the largest sum and return its sum.
+ * 
+ * Example:
+ * Input: [-2, 1, -3, 4, -1, 2, 1, -5, 4],
+ * Output: 6
+ * Explanation: [4, -1, 2, 1] has the largest sum = 6.
+ * 
+ * Follow up:
+ * If you have figured out the O(n) solution, try coding another solution
+ * using the divide and conquer approach, which is more subtle.
+ * ##########################################################################
+ * 
+ * File:   maximumSubarray.cpp
+ * 
+ * Author: Meisam Amjad amjadm@miamioh.edu
+ * 
+ * Time in leetCode =  ms
+ * Rank in leetCode = beats % in that runtime range.
+ */
+
+#include <assert.h>
+#include <iostream>
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        return 0;
+    }
+};
+
+int main(int argc, char** argv) {
+    return 0;
+}
+
