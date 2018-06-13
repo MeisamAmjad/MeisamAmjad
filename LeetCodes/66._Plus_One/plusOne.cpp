1883d929f1e32a01b43c82d34be770b1442a32b3
meisamamjad <amjadm@miamioh.edu>
2018-06-13 17:45:17 -0400

    just started working on this problem

---
+++ LeetCodes/66._Plus_One/plusOne.cpp
@@ -0,0 +1,45 @@
+/* Copyright (C) 2018 amjadm@miamiOH.edu
+ * 
+ * ##########################################################################
+ * Given a non-empty array of digits representing a non-negative integer, 
+ * plus one to the integer.
+ * The digits are stored such that the most significant digit is at the head
+ * of the list, and each element in the array contain a single digit.
+ * 
+ * You may assume the integer does not contain any leading zero, except the
+ * number 0 itself.
+ * 
+ * Example 1:
+ * Input: [1, 2, 3]
+ * Output: [1, 2, 4]
+ * Explanation: The array represents the integer 123.
+ * 
+ * Example 2:
+ * Input: [4, 3, 2, 1]
+ * Output: [4, 3, 2, 2]
+ * Explanation: The array represents the integer 4321.
+ * ##########################################################################
+ * 
+ * File:   plusOne.cpp
+ * 
+ * Author: Meisam Amjad amjadm@miamioh.edu
+ * 
+ * Time in leetCode =  ms
+ * Rank in leetCode = beats % in that runtime range.
+ */
+
+#include <assert.h>
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> plusOne(std::vector<int>& digits) {
+        return {0};
+    }
+};
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
+
