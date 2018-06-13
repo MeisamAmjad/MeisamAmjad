059a582d56b7f40193cff1b005141ed7ff5fccbc
meisamamjad <amjadm@miamioh.edu>
2018-06-13 17:10:46 -0400

    just startet working on this problm

---
+++ LeetCodes/58_Length_of_Last_Word/lengthOfLastWord.cpp
@@ -0,0 +1,30 @@
+/* Copyright (C) 2018 amjadm@miamiOH.edu
+ * 
+ * ##########################################################################
+ * Given a string s consists of upper/lower-case alphabets and empty space 
+ * characters ' ', return the length of last word in the string.
+ * If the last word does not exist, return 0.
+ * 
+ * Note: A word is defined as a character sequence consists of non-space
+ * characters only.
+ * 
+ * Example:
+ * Input: "Hello World"
+ * Output: 5
+ * ##########################################################################
+ * 
+ * File:   lengthOfLastWord.cpp
+ * 
+ * Author: Meisam Amjad amjadm@miamioh.edu
+ * 
+ * Time in leetCode =  ms
+ * Rank in leetCode = beats % in that runtime range.
+ */
+
+#include <assert.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
+
