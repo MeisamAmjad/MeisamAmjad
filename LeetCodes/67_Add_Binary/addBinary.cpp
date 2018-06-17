77a93e76ddafcef0f4501b3d8ac4e9163b5b1889
meisamamjad <amjadm@miamioh.edu>
2018-06-17 04:31:59 -0400

    just started working on this

---
+++ LeetCodes/67_Add_Binary/addBinary.cpp
@@ -0,0 +1,30 @@
+/* Copyright (C) 2018 amjadm@miamiOH.edu
+ * 
+ * ##########################################################################
+ * Given two binary strings, return their sum (also a binary string).
+ * The input strings are both non-empty and contains only characters 1 or 0.
+ * 
+ * Example 1:
+ * Input: a = "11", b = "1"
+ * Output: "100"
+ * 
+ * Example 2:
+ * Input: a = "1010", b = "1011"
+ * Output: "10101"
+ * ##########################################################################
+ * 
+ * File:   addBinary.cpp
+ * 
+ * Author: Meisam Amjad amjadm@miamioh.edu
+ * 
+ * Time in leetCode =  ms
+ * Rank in leetCode = beats % in that runtime range.
+ */
+
+#include <assert.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
+
