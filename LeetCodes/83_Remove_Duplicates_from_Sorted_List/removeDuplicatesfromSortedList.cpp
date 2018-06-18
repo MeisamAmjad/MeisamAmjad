b9cada36720c852cd5cba61ebc38852dc8f3ce85
meisamamjad <amjadm@miamioh.edu>
2018-06-17 23:36:58 -0400

    finished the code and had it submitted in leetcode

--- LeetCodes/83_Remove_Duplicates_from_Sorted_List/removeDuplicatesfromSortedList.cpp
+++ LeetCodes/83_Remove_Duplicates_from_Sorted_List/removeDuplicatesfromSortedList.cpp
@@ -17,12 +17,18 @@
  * 
  * Author: Meisam Amjad amjadm@miamioh.edu
  * 
- * Time in leetCode =   ms
- * Rank in leetCode = beats % in that runtime range.
+ * Time in leetCode =  10 ms
+ * Rank in leetCode = beats 100 % in that runtime range.
  */
 
 #include <assert.h>
 #include <iostream>
+
+static auto ___ = []() { 
+    std::ios::sync_with_stdio(false); 
+    std::cin.tie(NULL);  
+    return 0; 
+}();
 
 struct ListNode {
     int val;
@@ -32,7 +38,17 @@ struct ListNode {
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        return NULL;
+        ListNode *now = head, *tmp;
+        while (head && now->next) {
+            if (now->val == now->next->val) {
+                tmp = now -> next;
+                now->next = now->next->next;
+                delete tmp;
+            } else {
+                now = now->next;
+            }
+        }
+        return head;
     }
 };
 
