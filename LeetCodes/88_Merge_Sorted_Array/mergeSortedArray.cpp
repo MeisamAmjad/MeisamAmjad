6a20aa647fcd57a19c08aaf761f71f33af17e74d
meisamamjad <amjadm@miamioh.edu>
2018-06-18 22:28:22 -0400

    finished the code and submited in leetcode

--- LeetCodes/88_Merge_Sorted_Array/mergeSortedArray.cpp
+++ LeetCodes/88_Merge_Sorted_Array/mergeSortedArray.cpp
@@ -22,11 +22,47 @@
  * 
  * Author: Meisam Amjad amjadm@miamioh.edu
  * 
- * Time in leetCode =   ms
- * Rank in leetCode = beats  % in that runtime range.
+ * Time in leetCode =  5 ms
+ * Rank in leetCode = beats 100 % in that runtime range.
  */
 
 #include <assert.h>
 #include <iostream>
+#include <vector>
 
+static auto ___ = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
+        int i = m - 1, j = n - 1, index = nums1.size() - 1;
+        for (; i >= 0 && j >= 0; --index) {        
+            if (nums1[i] > nums2[j])
+                nums1[index] = nums1[i--];
+            else
+                nums1[index] = nums2[j--];
+        }
+        for (; i >= 0; nums1[index--] = nums1[i--]) {}
+        for (; j >= 0; nums1[index--] = nums2[j--]) {}
+    }
+};
+
+int main(int argc, char** argv) {
+    Solution test;
+    std::vector<int> a = {1, 2, 3, 0, 0, 0}; 
+    std::vector<int> b = {2, 5, 6};
+    std::vector<int> result = {1, 2, 2, 3, 5, 6};
+    test.merge(a, 3, b, 3);
+    assert(a == result);
+    a = {0, 0, 3, 0, 0, 0, 0, 0, 0};
+    b = {-1, 1, 1, 1, 2, 3};
+    result = {-1, 0, 0, 1, 1, 1, 2, 3, 3};
+    test.merge(a, 3, b, 6);
+    assert(a == result);
+    return 0;
+}
 
