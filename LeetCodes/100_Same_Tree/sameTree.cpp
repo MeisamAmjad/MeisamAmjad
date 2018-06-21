10cb467bf4a64c0ba635ab26a435bd60e5751f88
meisamamjad <amjadm@miamioh.edu>
2018-06-21 11:27:06 -0400

    just started new project

---
+++ LeetCodes/100_Same_Tree/sameTree.cpp
@@ -0,0 +1,49 @@
+/* Copyright (C) 2018 amjadm@miamiOH.edu
+ * 
+ * ##########################################################################
+ * Given two binary trees, write a function to check if they are the same 
+ * or not.
+ * 
+ * Two binary trees are considered the same if they are structurally identical
+ * and the nodes have the same value.
+ * 
+ * Example 1:
+ * Input:     1         1
+ *           / \       / \
+ *          2   3     2   3
+ * 
+ *         [1,2,3],   [1,2,3]
+ * Output: true
+ * 
+ * Example 2:
+ * Input:     1         1
+ *           /           \
+ *          2             2
+ * 
+ *         [1,2],     [1,null,2]
+ * Output: false
+ * 
+ * Example 3:
+ * Input:     1         1
+ *           / \       / \
+ *          2   1     1   2
+ * 
+ *         [1,2,1],   [1,1,2]
+ * Output: false
+ * ##########################################################################
+ * 
+ * File:   sameTree.cpp
+ * 
+ * Author: Meisam Amjad amjadm@miamioh.edu
+ * 
+ * Time in leetCode =   ms
+ * Rank in leetCode = beats  % in that runtime range.
+ */
+
+#include <assert.h>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    return 0;
+}
+
