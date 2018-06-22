9c805fec39186ab9a10824d876bac108ff33f624
meisamamjad <amjadm@miamioh.edu>
2018-06-21 20:20:26 -0400

    finished and submitted on leetcode

--- LeetCodes/104_Maximum_Depth_Of_Binary_Tree/maximumDepthOfBinaryTree.cpp
+++ LeetCodes/104_Maximum_Depth_Of_Binary_Tree/maximumDepthOfBinaryTree.cpp
@@ -22,19 +22,34 @@
  * 
  * Author: Meisam Amjad amjadm@miamioh.edu
  * 
- * Time in leetCode =   ms
- * Rank in leetCode = beats  % in that runtime range.
+ * Time in leetCode =  5 ms
+ * Rank in leetCode = beats 99.84 % in that runtime range.
  */
 
 #include <assert.h>
 #include <iostream>
 #include <queue>
+#include <algorithm>
 
 static auto ___ = []() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
     return NULL;
 }();
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        return 1 + std::max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
 
 int main(int argc, char* argv[]) {
     return 0;
