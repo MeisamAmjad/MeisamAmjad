fc681f603b1b0bf35b717a076dfdaa6dc21116b2
meisamamjad <amjadm@miamioh.edu>
2018-06-21 20:18:22 -0400

    just started this project

---
+++ LeetCodes/104_Maximum_Depth_Of_Binary_Tree/maximumDepthOfBinaryTree.cpp
@@ -0,0 +1,42 @@
+/* Copyright (C) 2018 amjadm@miamiOH.edu
+ * 
+ * ##########################################################################
+ * Given a binary tree, find its maximum depth.
+ * 
+ * The maximum depth is the number of nodes along the longest path from the
+ * root node down to the farthest leaf node.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * return its depth = 3.
+ * ##########################################################################
+ * 
+ * File:   maximumDepthOfBinaryTree.cpp
+ * 
+ * Author: Meisam Amjad amjadm@miamioh.edu
+ * 
+ * Time in leetCode =   ms
+ * Rank in leetCode = beats  % in that runtime range.
+ */
+
+#include <assert.h>
+#include <iostream>
+#include <queue>
+
+static auto ___ = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    return NULL;
+}();
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
+
