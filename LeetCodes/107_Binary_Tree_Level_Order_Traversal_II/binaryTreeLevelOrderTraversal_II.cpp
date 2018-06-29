7328f1f7cb64b0125662719d68c16dc1971d6588
meisamamjad <amjadm@miamioh.edu>
2018-06-29 18:43:56 -0400

    just started this project

---
+++ LeetCodes/107_Binary_Tree_Level_Order_Traversal_II/binaryTreeLevelOrderTraversal_II.cpp
@@ -0,0 +1,45 @@
+/* Copyright (C) 2018 amjadm@miamiOH.edu
+ * 
+ * ##########################################################################
+ * Given a binary tree, return the bottom-up level order traversal of its
+ * nodes' values. (ie, from left to right, level by level from leaf to root).
+ * 
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 
+ * return its bottom-up level order traversal as:
+ * [
+ *   [15,7],
+ *   [9,20],
+ *   [3]
+ * ]
+ * ##########################################################################
+ * 
+ * File:   binaryTreeLevelOrderTraversal_II.cpp
+ * 
+ * Author: Meisam Amjad amjadm@miamioh.edu
+ * 
+ * Time in leetCode =   ms
+ * Rank in leetCode = beats  % in that runtime range.
+ */
+
+#include <assert.h>
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+static auto ___ = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    return NULL;
+}();
+
+int main(int argc, char** argv) {
+    return 0;
+}
+
