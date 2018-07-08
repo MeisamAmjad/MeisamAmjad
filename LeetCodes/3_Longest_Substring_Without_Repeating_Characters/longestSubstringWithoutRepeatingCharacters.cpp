6d1ed14d259500be14a13018ee66a9d491b101bb
meisamamjad <amjadm@miamioh.edu>
2018-07-08 18:01:45 -0400

    just started this project

---
+++ LeetCodes/3_Longest_Substring_Without_Repeating_Characters/longestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,46 @@
+/* Copyright (C) 2017 amjadm@miamiOH.edu
+ * 
+ * ##########################################################################
+ * Given a string, find the length of the longest substring without repeating
+ * characters.
+ * 
+ * Examples:
+ * 
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ * 
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ * 
+ * Given "pwwkew", the answer is "wke", with the length of 3. Note that the
+ * answer must be a substring, "pwke" is a subsequence and not a substring.
+ * 
+ * ##########################################################################
+ * 
+ * File:   longestSubstringWithoutRepeatingCharacters.cpp
+ * 
+ * Author: Meisam Amjad amjadm@miamioh.edu
+ * 
+ * Time in leetCode =  ms
+ * Rank in leetCode = beats  % in that runtime range.
+ */
+
+#include <assert.h>
+#include <iostream>
+#include <string>
+
+static auto ___ = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(std::string s) {
+        return 0;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
+
