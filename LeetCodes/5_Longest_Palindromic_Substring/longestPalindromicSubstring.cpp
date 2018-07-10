197a294b2abcc4fc4094f1dcca383e0805d1a182
meisamamjad <amjadm@miamioh.edu>
2018-07-09 21:16:18 -0400

    just started working on this project

---
+++ LeetCodes/5_Longest_Palindromic_Substring/longestPalindromicSubstring.cpp
@@ -0,0 +1,38 @@
+/* Copyright (C) 2017 amjadm@miamiOH.edu
+ * 
+ * ##########################################################################
+ * Given a string s, find the longest palindromic substring in s. You may 
+ * assume that the maximum length of s is 1000.
+ * 
+ * Example 1:
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ * 
+ * Example 2:
+ * Input: "cbbd"
+ * Output: "bb"
+ * 
+ * ##########################################################################
+ * 
+ * File:   longestPalindromicSubstring.cpp
+ * 
+ * Author: Meisam Amjad amjadm@miamioh.edu
+ * 
+ * Time in leetCode =  ms
+ * Rank in leetCode = beats  % in that runtime range.
+ */
+
+#include <assert.h>
+#include <iostream>
+
+static auto ___ = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    return 0;
+}();
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
+
